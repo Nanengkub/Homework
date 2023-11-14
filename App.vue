17a2d350634237e95a13e0b9a10fc0e49a0c20a4
napapha_nan <148744310+Nanengkub@users.noreply.github.com>
2023-11-14 23:03:38 +0700

    work2

--- App.vue
+++ App.vue
@@ -1,20 +1,30 @@
 <template>
-  <TestPage />
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+    <img v-bind:src="picture" width="300" height="400">
+    <h1>ชื่อผู้สมัครงาน : {{fullname()}}</h1>
+    <h1>อายุ : {{age}} ปี</h1>
+    <p>ที่อยู่ : <span v-html="address"></span></p>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TestPage from './components/TestPage.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-    TestPage
+  data(){
+    return{
+      firstname:"ณปภา",
+      lastname:"ธุวสินธุ์",
+      age:17,
+      address:"<i>จังหวัดฉะเชิงเทรา</i>",
+      picture:"https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.15752-9/262418668_3102586646696924_7027298862749235368_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8cd0a2&_nc_ohc=EGu8Wv8s7XUAX9MFuYT&_nc_ht=scontent.fbkk5-3.fna&oh=03_AdTO9wJDHPgJT7WvwoV3HNaqsShQM3bUk2p2lSe3aitoRg&oe=657B0714"
+    }
+  },
+  methods:{
+    fullname(){
+      return this.firstname + " "+ this.lastname
+    }
   }
-}
+  }
 </script>
 
 <style>
