783b310a2d98e2fd9fd4c0008429b8f84ec3414e
napapha_nan <148744310+Nanengkub@users.noreply.github.com>
2023-12-11 20:57:38 +0700

    work5

---
+++ DetailView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h2>--Detail--</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {};
+    },
+
+};
+</script>
+
+<style scoped lang="sc55"></style>
